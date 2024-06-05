a155bbfb9ae9cd074ab4ac642bb1197a2b910ae8
Muskan Swarup <muskanswarup680@gmail.com>
2024-06-05 23:58:18 +0530

    modified functions

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ let compScore = document.querySelector("#computer-score");
 
 const choices = document.querySelectorAll(".choice");
 
-// const playGames() =>{
+const computerChoice = () =>{
+    const options = ["rock" , "paper", "scissors"];
+    const randomIndex =  Math.floor(Math.random() * 3);   
+    return options[randomIndex];
+}
 
-// }
+
+const playGames = (userChoice) =>{
+    console.log("User choice:", userChoice);
+    const compChoice = computerChoice();
+    console.log("Computer choice :" , compChoice)
+
+    
+}
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
-        const choiceId = choice.querySelector("img").getAttribute("id");
-        console.log(choiceId, "was clicked");
-        playGames();
+        const userChoice = choice.querySelector("img").getAttribute("id");
+        console.log(userChoice, "was clicked");
+        playGames(userChoice);
     });
 });
