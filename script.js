f024a26b2096ea1d4225ffd29132939da0da129a
Muskan Swarup <muskanswarup680@gmail.com>
2024-06-06 00:21:27 +0530

    completed functionality.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let userScore = document.querySelector("#user-score");
 let compScore = document.querySelector("#computer-score");
+let displayBoard = document.querySelector("#msg");
+
+let uScore = 0;
+let cScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
@@ -9,19 +13,55 @@ const computerChoice = () =>{
     return options[randomIndex];
 }
 
+const drawGame = ()=>{
+    console.log("Game was draw");
+}
+
+const showWinner = (userWin) =>{
+    if(userWin){
+        uScore++;
+        console.log("You won!");
+        displayBoard.innerText = `You win!`;
+        displayBoard.style.backgroundColor = "green";
+        displayBoard.style.color = "blanchedalmond";
+
+
+        userScore.innerText = uScore;
+    }else{
+        cScore++;
+        console.log("You lost!");
+        displayBoard.innerText = "You lose!";
+        displayBoard.style.backgroundColor = "red";
+        displayBoard.style.color = "blanchedalmond";
+        compScore.innerText = cScore;
+    }
+}
 
 const playGames = (userChoice) =>{
     console.log("User choice:", userChoice);
     const compChoice = computerChoice();
     console.log("Computer choice :" , compChoice)
 
+    if(userChoice == compChoice){
+        drawGame();
+    }else{
+        let userWin = true;
+
+        if(userChoice == "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        }else if(userChoice == "paper"){
+            userWin = compChoice === "scissors" ? false : true;
+        }else{
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin);
+    }
     
 }
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
         const userChoice = choice.querySelector("img").getAttribute("id");
-        console.log(userChoice, "was clicked");
         playGames(userChoice);
     });
 });
